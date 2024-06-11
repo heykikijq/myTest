2cef1b88e82e287e3ce2331f216e8aaa0f7a98a4
heykikijq <126369689+heykikijq@users.noreply.github.com>
2024-06-11 10:19:05 +0800

    Add files via upload

---
+++ SpringLesson06/SpringLesson06/src/test/java/com/example/springlesson06/SpringLesson06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlesson06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLesson06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
